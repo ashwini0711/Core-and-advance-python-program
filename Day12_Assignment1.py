4c7f38810323be8df452c5f7e448f0ffe0e2e204
ashwini0711 <ashwinipawar75760@gmail.com>
2025-02-21 12:36:00 +0530

    Add files via upload

---
+++ Day12_Assignment1.py
@@ -0,0 +1,11 @@
+#Program to find the occurance of 4 in the tuple
+#Defining the tuple
+t = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+#Count occurrences of 4
+count = t.count(4)
+#Print the result
+print("Number of times 4 appears in the tuple:", count)
+
+'''Output
+Number of times 4 appears in the tuple: 3
+'''
---
+++ Day12_Assignment2.py
@@ -0,0 +1,13 @@
+#program to convert a list into a tuple
+#Defining the list
+l1 = [5, 10, 7, 4, 15, 3]
+#Convert the list to a tuple
+tup = tuple(l1)
+#Print the result
+print("Tuple:", tup)
+
+'''Output
+Tuple: (5, 10, 7, 4, 15, 3)
+'''
+
+
